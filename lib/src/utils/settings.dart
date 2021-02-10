1ab49566645967cea19af8b1e9690037eb20bf27
Justin Suderman <jsuderman1@gmail.com>
2021-02-10 10:58:39 -0800

    added key and token

--- lib/src/utils/settings.dart
+++ lib/src/utils/settings.dart
@@ -1,3 +1,4 @@
 /// Define App ID and Token
-const APP_ID = Your_App_ID;
-const Token = Your_Token;
+const APP_ID = '864d7ed94a994675a3a8931cdf0611bc';
+const Token =
+    '006864d7ed94a994675a3a8931cdf0611bcIADkj6Z/stwuhAorpJ2YxSQqkG10gcfWOAhB9rJY2hPIFWjYiBkAAAAAEABoKgnHDH4lYAEAAQAMfiVg';
